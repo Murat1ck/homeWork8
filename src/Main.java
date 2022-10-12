16e9cacaa86e6ebd6199418c2a60065f14d6087e
Murat1ck <muratoff101088@gmail.com>
2022-10-12 15:52:02 +0300

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,43 @@
 public class Main {
     public static void main(String[] args) {
-        int[] weights = {90, 91, 93, 92, 85, 87, 88, 89, 0, 0, 0, 0};
-        weights[0] = 90;
-        int januaryWeight = weights[0];
-        System.out.println(januaryWeight);
-        System.out.println(weights[0]);
-        System.out.println(weights[4]);
-        int january = 0;
-        System.out.println(weights[january]);
-        for (int i = 0; i < weights.length; i++) {
-            System.out.println(weights[i]);
-        }
-    }
-        public static void task1 () {
+        // int[] weights = {90, 91, 93, 92, 85, 87, 88, 89, 0, 0, 0, 0};
+        //  weights[0] = 90;
+        // int januaryWeight = weights[0];
+        // System.out.println(januaryWeight);
+        //  System.out.println(weights[0]);
+        //  System.out.println(weights[4]);
+        //  int january = 0;
+        //  System.out.println(weights[january]);
+        //  for (int i = 0; i < weights.length; i++) {
+        //     System.out.println(weights[i]);
+        //  }
+        //}
+
+        // public static void task1() {
+        //  int[] weight = new int[3];
+        // weight[0] = 1;
+        //  weight[1] = 2;
+        ///  weight[2] = 3;
+        // double[] fl = {1.57, 7.654, 9.986};
+        // int[] a = {1, 2, 3, 4, 5, 6};
+        //  System.out.println(fl[2]);
+
+
+        // }
+
+       // public static void task2() {
             int[] weight = new int[3];
             weight[0] = 1;
             weight[1] = 2;
             weight[2] = 3;
-            double[] fl = {1.57, 7.654, 9.986};
-            int[] a = {1, 2, 3, 4, 5, 6};
-            System.out.println(fl[2]);
-
+            float[] fl = new float[]{1.57f, 7.654f, 9.986f};
+            int[] a = new int[]{1, 2, 3, 4, 5, 6};
+            System.out.printf("%d,%d,%d\n", weight[0],weight[1],weight[2]);
+            System.out.printf("%f,%f,%f\n", fl[0],fl[1],fl[2]);
+            System.out.printf("%d,%d,%d\n", a[0],a[1],a[2],a[3],a[4],a[5]);
 
         }
 }
+
+
 
