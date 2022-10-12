a51b17237b65a6d96950f5561c4036a350090796
Murat1ck <muratoff101088@gmail.com>
2022-10-12 15:14:53 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] weights = {90, 91, 93, 92, 85, 87, 88, 89, 0, 0, 0, 0};
+        weights[0] = 90;
+        int januaryWeight = weights[0];
+        System.out.println(januaryWeight);
+        System.out.println(weights[0]);
+        System.out.println(weights[4]);
+        int january = 0;
+        System.out.println(weights[january]);
+        for (int i = 0; i < weights.length; i++) {
+            System.out.println(weights[i]);
+        }
     }
+        public static void task1 () {
+            int[] weight = new int[3];
+            weight[0] = 1;
+            weight[1] = 2;
+            weight[2] = 3;
+            double[] fl = {1.57, 7.654, 9.986};
+            int[] a = {1, 2, 3, 4, 5, 6};
+            System.out.println(fl[2]);
+
+
+        }
 }
+
